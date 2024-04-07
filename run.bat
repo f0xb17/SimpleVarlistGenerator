930b336b7d7475afeb6bd8c8d7676844ab55f9a0
f0xb17 <102655383+f0xb17@users.noreply.github.com>
2024-04-07 13:43:10 +0200

    Update run.bat
    Added the possibility to rerun the script after finishing.

--- run.bat
+++ run.bat
@@ -5,7 +5,13 @@ if errorlevel 1 goto errorNoPython
 
 REM Reaching here means Python is installed
 REM Execute your Python script
+:ReRun
 python main.py
+
+
+REM After finishing just rerun the same script again.
+REM jump to rerun
+goto ReRun
 
 :errorNoPython
 echo Python is not installed. Please install Python and try again.
