ae200e58295fbf38ca1bdffc330b3ba0ef5aab0d
f0xb17 <102655383+f0xb17@users.noreply.github.com>
2024-04-26 10:09:09 +0200

    File extension is now variable FindFilesInFolder method no longer has a static file extension and can now be used for different file types.

--- src/main/kotlin/lib/File.kt
+++ src/main/kotlin/lib/File.kt
@@ -20,11 +20,12 @@ class File(private val dir: String, private val option: Int) {
     /**
      * This method is used to find all files with the extension .osc at the specified storage point
      * and save them in a list.
+     * @param [suffix] The File extension to be searched for. (e.g. .osc)
      * @return a List of Strings, which contains all files with the ending .osc in a specific folder.
      */
-    private fun findFilesInFolder(): List<String> {
+    private fun findFilesInFolder(suffix: String): List<String> {
         val files = File(this.dir).walk()
-            .filter { it.isFile && it.name.endsWith(".osc") }
+            .filter { it.isFile && it.name.endsWith(suffix) }
                 .toList()
                     .map { it.absolutePath }
         return files
