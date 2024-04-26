c7c9f599f511076473116cdaad964a694ec34d6f
f0xb17 <102655383+f0xb17@users.noreply.github.com>
2024-04-26 10:02:11 +0200

    Created File.kt Class file created. Class File contains a constructor for the file path and option to be executed.

--- src/main/kotlin/lib/File.kt
+++ src/main/kotlin/lib/File.kt
@@ -1,4 +1,12 @@
+/**
+ * libfile.kt
+ * Extracts all variables from an OMSI script file (.osc) and saves them in a newly created varlist or stringvarlist.
+ *
+ * Created by f0xb17 on 04/21/2024.
+ * Copyright © 2024 f0xb17. All rights reserved.
+ */
+
 package org.foxbit.lib
 
-class File {
+class File(private val dir: String, private val option: Int) {
 }
