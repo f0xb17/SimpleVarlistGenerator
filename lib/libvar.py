c5e1d830c9c8b34d6d8544bfd967c1c4ca25b19d
f0xb17 <102655383+f0xb17@users.noreply.github.com>
2024-04-26 09:55:29 +0200

    Clean folder                                      Python structure removed.

--- lib/libvar.py
+++
@@ -1,8 +0,0 @@
-import re
-
-
-class LibVar:
-    @staticmethod
-    def extractvarname(varnames):
-        matches = re.findall(r"\((S\.L|L\.L)\.(\w+)\)", varnames)
-        return [match[1].lower()for match in matches]
