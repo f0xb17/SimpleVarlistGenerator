ac997dfad4b7a81e1290dde4e19e6c4e28ecb42d
foxb17 <102655383+f0xb17@users.noreply.github.com>
2025-01-24 19:06:22 +0100

    Added Methods
    Added methods to clean OMSI variables with regex pattern. Also created
    clean up method to make a list duplicate free.

---
+++ main.py
@@ -0,0 +1,33 @@
+import re
+def convert_variable(variable):
+    """
+    Takes a variable name and returns the name without any unnecessary characters.
+    It looks for a string in the format "(L.L.variable)" and returns "variable".
+    If the string is not in this format, a ValueError is raised with a message describing
+    the invalid variable.
+    """
+    match = re.match(r'\(L\.([L$])\.([a-zA-Z0-9_]+)\)', variable)
+    if match:
+        return match.group(2)
+    raise ValueError(f"Invalid variable: {variable}")
+
+def create_list(variable_list):
+  """
+  Takes a list of variable names and returns a new list with all duplicates
+  removed. The list is cleaned of unnecessary characters by passing each
+  variable through the convertVariable function.
+  """
+  cleaned_list = []
+  for variable in variable_list:
+    variable = convert_variable(variable)
+    if variable not in cleaned_list:
+      cleaned_list.append(variable)
+  return cleaned_list
+  
+def main():
+  variabe_list_in = ["(L.L.variable)", "(L.L.variable1)", "(L.L.variable2)", "(L.L.variable3)", "(L.L.variable4)", "(L.L.variable3)"]
+  variable_list = ["a", "b", "c", "d", "e", "f", "g", "h", "a", "b", "d", "e"]
+  print(create_list(variabe_list_in))
+
+if __name__ == "__main__":
+  main()
