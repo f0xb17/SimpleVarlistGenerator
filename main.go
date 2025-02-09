c3edce2d6d5009f93110b94ddd01adf60cdfbc5f
f0xb17 <102655383+f0xb17@users.noreply.github.com>
2025-02-09 18:26:58 +0100

    Added cleanSlice Method
    This method removes duplicate variable names from the input slice.

--- main.go
+++ main.go
@@ -12,6 +12,25 @@ func convertVariable(varName string) string {
 	return match[2]
 }
 
+// cleanSlice removes duplicate variable names from the input slice by extracting
+// the "real" variable name using convertVariable and returns a slice of unique
+// variable names.
+func cleanSlice(varSlice []string) []string {
+	cleaned := make(map[string]bool)
+	cleanedSlice := []string{}
+
+	for _, v := range varSlice {
+		varName := convertVariable(v)
+		cleaned[varName] = true
+	}
+
+	for v := range cleaned {
+		cleanedSlice = append(cleanedSlice, v)
+	}
+
+	return cleanedSlice
+}
+
 func main() {
 	fmt.Println("Hello, World!")
 }
