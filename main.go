10cd4debed78e1d94f2dee91403c5c50f2e58678
f0xb17 <102655383+f0xb17@users.noreply.github.com>
2025-02-09 18:25:59 +0100

    Added convertVariable Method
    This method returns the part of the name after the <L.L.> or <L.$.>.

--- main.go
+++ main.go
@@ -1,6 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"regexp"
+)
+
+// convertVariable takes a variable name and returns the part of the name after the <L.L.> or <L.$.>.
+// part. It is used to extract the "real" variable name.
+func convertVariable(varName string) string {
+	match := regexp.MustCompile(`\(L\.([L$])\.([a-zA-Z0-9_]+)\)`).FindStringSubmatch(varName)
+	return match[2]
+}
 
 func main() {
 	fmt.Println("Hello, World!")
