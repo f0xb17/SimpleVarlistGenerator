5c0fadc75ea8cd9839b7ddab99129a9ce90acf1d
f0xb17 <102655383+f0xb17@users.noreply.github.com>
2025-02-09 18:29:39 +0100

    Added createSlices Method
    This Method takes a slice of strings and splits it into two slices =
    Varlist, Stringvarlist.

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"regexp"
+	"strings"
 )
 
 // convertVariable takes a variable name and returns the part of the name after the <L.L.> or <L.$.>.
@@ -31,6 +33,48 @@ func cleanSlice(varSlice []string) []string {
 	return cleanedSlice
 }
 
+// createSlices takes a slice of strings and splits it into two slices. The first slice contains
+// all the strings that contained <L.$.> (Stringvars) and the second slice contains all the strings that
+// contained <L.L.> (Vars). The function returns the two slices as well as an error if the input slice
+// is empty or the string contains no letters.
+func createSlices(varSlice []string) ([]string, []string, error) {
+	stringvarlist := []string{}
+	varlist := []string{}
+
+	if len(varSlice) == 0 {
+		return nil, nil, errors.New("error while creating slices. input slice was empty")
+	}
+
+	for _, v := range varSlice {
+		if v == "" {
+			return nil, nil, errors.New("string contains no letters. Is it empty")
+		}
+
+		if strings.Contains(v, "$") {
+			stringvarlist = append(stringvarlist, v)
+		} else {
+			varlist = append(varlist, v)
+		}
+
+	}
+
+	return cleanSlice(stringvarlist), cleanSlice(varlist), nil
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	testSlice := []string{"(L.$.DasIstEineSehrLangeTestVariable)", "(L.L.DasIstEineSehrLangeTestVariable2)", "(L.$.DasIstEineSehrLangeTestVariable3)", "(L.L.DasIstEineSehrLangeTestVariable4)", "(L.L.DasIstEineSehrLangeTestVariable2)", "(L.L.DasIstEineSehrLangeTestVariable3)", "(L.L.DasIstEineSehrLangeTestVariable4)"}
+	newSlice, newSlice2, err := createSlices(testSlice)
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+
+	fmt.Println("Varlist:")
+	for _, x := range newSlice2 {
+		fmt.Println(x)
+	}
+
+	fmt.Println("Stringvarlist:")
+	for _, x := range newSlice {
+		fmt.Println(x)
+	}
 }
