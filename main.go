0c2ce68a46658aa60af8f58c2b02388e3f86a898
f0xb17 <102655383+f0xb17@users.noreply.github.com>
2025-02-12 11:57:36 +0100

    Added documentation

--- main.go
+++ main.go
@@ -20,18 +20,24 @@ type Variable struct {
 	Type string
 }
 
+// getName returns the name of the variable.
 func (v Variable) getName() string {
 	return v.Name
 }
 
+// getType returns the type of the variable, which is either "variable" or "stringvariable".
 func (v Variable) getType() string {
 	return v.Type
 }
 
+// print returns a string representation of the Variable in the format "Name: Type"
 func (v Variable) print() string {
 	return v.getName() + ": " + v.getType()
 }
 
+// collectVariables reads a file and collects all variables in the format (L|S)\.(L|\$)([a-zA-Z0-9_]+)
+// into a map. The key of the map is an object from type Variable and the value is always true.
+// The function returns an error if the file path is empty or if there was an error opening the file.
 func collectVariables(filePath string) (map[Variable]bool, error) {
 	if filePath == "" {
 		return nil, fmt.Errorf("file path is empty")
@@ -65,6 +71,9 @@ func collectVariables(filePath string) (map[Variable]bool, error) {
 	return variables, nil
 }
 
+// separateVariables takes a map of variables and separates them into two lists.
+// The first list contains all "variable"s and the second list contains all "stringvariable"s.
+// The function returns an error if the map is empty or if there is an unknown or empty type in the map.
 func separateVariables(varMap map[Variable]bool) ([]Variable, []Variable, error) {
 	if len(varMap) < 1 {
 		return nil, nil, errors.New("map is empty")
